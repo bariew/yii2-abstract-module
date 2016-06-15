14ae17bf69ee337b85806ff53850425172db984b
Pavel Bariev <bariew@yandex.ru>
2016-06-15 22:11:02 +0400

    working version

--- controllers/AbstractModelController.php
+++ controllers/AbstractModelController.php
@@ -7,11 +7,9 @@
 
 namespace bariew\abstractModule\controllers;
 
-use bariew\abstractModule\models\AbstractModel;
 use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use bariew\abstractModule\Module;
 
 /**
  * For managing abstract items.
@@ -20,18 +18,22 @@ use bariew\abstractModule\Module;
  */
 class AbstractModelController extends Controller
 {
+    public $modelName = '$2';
     /**
      * Finds the Item model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer|boolean $id
      * @param boolean $search
      * @return \yii\db\ActiveRecord the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
+     * @throws NotFoundHttpException
      */
     public function findModel($id = false, $search = false)
     {
-        $class = preg_replace('#^(.+)/controllers/(.+)Controller#', '$1/models/$2', static::className())
-            . ($search ? 'Search' : '');
+        $class = preg_replace(
+            '#^(.+)\\\\controllers\\\\(.+)Controller#',
+            '$1\\\\models\\\\'.$this->modelName,
+            static::className()
+        ) . ($search ? 'Search' : '');
         $model = new $class();
         if ($id && (!$model = $model->search(compact('id'))->one())) {
             throw new NotFoundHttpException('The requested page does not exist.');
--- models/AbstractModel.php
+++ models/AbstractModel.php
@@ -7,7 +7,6 @@
 
 namespace bariew\abstractModule\models;
 
-use bariew\abstractModule\Module;
 use Yii;
 use \yii\db\ActiveRecord;
 use yii\db\ActiveQuery;
@@ -23,19 +22,35 @@ use yii\helpers\Inflector;
 class AbstractModel extends ActiveRecord
 {
     /**
+     * Gets class name for a parent model for the current module's model.
+     * @return static
+     */
+    public static function parentClass()
+    {
+        $parents = array_values(class_parents(get_called_class()));
+        $currentClass = $parents[array_search(get_parent_class(), $parents) - 2];
+        $pattern = '#^(.+\\\\)(\w+)$#';
+        $myFormName = preg_replace($pattern, '$2', $currentClass);
+        return preg_replace($pattern, '$1'.$myFormName, get_called_class());
+    }
+
+    /**
      * Gets class name for a model that inherits current modules model.
-     * @param $className
-     * @param null $formName
-     * @param array $replacements
+     * CAUTION! This works only when called from inside another module model
      * @return \yii\db\ActiveRecord
      */
-    public static function getClass($className = null, $formName = null, $replacements = [])
+    public static function childClass()
     {
-        $className = $className ? : static::className();
-        $class = $formName
-            ? preg_replace('/(.*\\\\)\w+$/', '$1' . $formName, $className)
-            : $className;
-        return str_replace(array_keys($replacements), array_values($replacements), $class);
+        $data = debug_backtrace();
+        $callingClassName = $data[1]['object']->className();
+        $pattern = '#^(.+\\\\)(\w+\\\\\w+)$#';
+        $formName = preg_replace($pattern, '$2', get_called_class());
+        return preg_replace($pattern, '$1'.$formName, $callingClassName);
+    }
+
+    public static function moduleName($className)
+    {
+        return preg_replace('#.*\\\\(\w+)\\\\\w+\\\\\w+$#','$1', $className);
     }
 
     /**
@@ -43,8 +58,8 @@ class AbstractModel extends ActiveRecord
      */
     public static function tableName()
     {
-        $moduleName = preg_replace('#.*\\\\(\w+)\\\\models\\\\\w+$#','$1', static::className());
-        return '{{%'.$moduleName . '_' . Inflector::camel2id(static::formName()).'}}';
+        $name = preg_replace(['#.*\\\\(\w+)\\\\models\\\\(\w+)$#', '#^(.+)Search$#'],['$1$2', '$1'], static::className());
+        return '{{%'.Inflector::camel2id($name, '_').'}}';
     }
 
     /**
@@ -56,5 +71,4 @@ class AbstractModel extends ActiveRecord
     {
         return $this::find()->andFilterWhere(array_merge($this->attributes, $params));
     }
-
 }
